eed0d84f38f8aebcb015a72f7c5ae1dd66dda4bd
pukhraj saini <pukhraj.saini@appinventiv.com>
2024-11-09 20:19:51 +0530

    availabilty rrsponse dto

--- src/app/api/halls/dto/availability.dto.ts
+++ src/app/api/halls/dto/availability.dto.ts
@@ -1,5 +1,7 @@
+import { ResponseDto } from "@app/api/response.dto";
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString } from "class-validator";
+import { Type } from "class-transformer";
+import { IsNumber, IsString } from "class-validator";
 
 export class HallAvailabilityQueryDto {
     @ApiProperty({
@@ -23,6 +25,79 @@ export class HallAvailabilityQueryDto {
         example: 191,
         required: true
     })
-    @IsString()
+    @IsNumber()
+    @Type(() => Number)
     noOfCandidates: number;
+}
+
+
+export class HallAvailabilitySlotDto {
+    @ApiProperty({
+        type: String,
+        example: '9a4523db-3a4e-4435-b8e4-5dc3e28613b0',
+        required: true
+    })
+    @IsString()
+    id: string;
+
+    @ApiProperty({
+        type: String,
+        example: '09:00 AM',
+    })
+    @IsString()
+    from: string;
+
+    @ApiProperty({
+        type: String,
+        example: '12:00 PM',
+    })
+    @IsString()
+    to: string;
+
+    @ApiProperty({
+        type: Number,
+        example: 191,
+        required: true
+    })
+    @IsNumber()
+    capacity: number;
+
+    @ApiProperty({
+        type: Boolean,
+        example: true,
+        required: true
+    })
+    @IsString()
+    isAvailable: true;
+}
+
+export class HallAvailabilityDateDto {
+    @ApiProperty({
+        type: String,
+        example: '2024-11-21',
+        required: true
+    })
+
+    @IsString()
+    date: string;
+
+    @ApiProperty({
+        type: HallAvailabilitySlotDto,
+    })
+    slots: HallAvailabilitySlotDto
+}
+
+
+export class HallAvailabilityResultDto {
+    @ApiProperty({
+        type: [HallAvailabilityDateDto],
+    })
+    availability: HallAvailabilityDateDto[]
+}
+
+export class HallAvailabilityResponseDto extends ResponseDto {
+    @ApiProperty({
+        type: HallAvailabilityResultDto
+    })
+    result: HallAvailabilityResultDto;
 }
