8e5f0dfec814052495fefad76e65a524defff845
pukhraj saini <pukhraj.saini@appinventiv.com>
2024-11-09 12:54:35 +0530

    booking module setup

--- src/app/api/api.module.ts
+++ src/app/api/api.module.ts
@@ -5,13 +5,15 @@ import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./users/user.module";
 import { TimeSlotModule } from "./time-slots/time-slot.module";
 import { HallModule } from "./halls/hall.module";
+import { BookingModule } from "./bookings/booking.module";
 
 @Module({
     imports: [
         UserModule,
         AuthModule,
         TimeSlotModule,
-        HallModule
+        HallModule,
+        BookingModule
     ],
     exports: [],
     controllers: [ApiController],
