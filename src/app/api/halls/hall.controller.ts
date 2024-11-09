235b668958682e35f1e282ab35a18e6d71b2234a
Abhijeetkumar24 <abhijeet.kumar@appinventiv.com>
2024-11-09 19:22:59 +0530

    Feat(): Create hall API

--- src/app/api/halls/hall.controller.ts
+++ src/app/api/halls/hall.controller.ts
@@ -2,28 +2,44 @@ import { COMMON_HEADERS } from "@app/app.constant";
 import { Message } from "@app/decorators";
 import { AuthGuard } from "@app/gaurds/auth.guard";
 import { LoggerService } from "@app/shared/logger";
-import { Controller, Get, Post, UseGuards } from "@nestjs/common";
-import { ApiBearerAuth, ApiHeaders, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiHeaders, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { HallService } from "./hall.service";
+import { CreateHallDto, CreateHallResponseDto } from "./dto/create.dto";
 
 @Controller({
-    path: 'hall',
-    version: '1'
+  path: 'hall',
+  version: '1'
 })
 @ApiHeaders(COMMON_HEADERS)
 @ApiTags('Halls')
 
 export class HallController {
 
-    constructor(private $logger: LoggerService) { }
+  constructor(
+    private $logger: LoggerService,
+    private readonly $hallService: HallService
+  ) { }
 
-    @Get('/availability')
-    @UseGuards(AuthGuard)
-    @ApiBearerAuth('AccessToken')
-    @Message('HALL.AVAILABILITY')
-    // @ApiOkResponse({ type: SendOtpResponseDto })
-    @ApiOperation({ summary: 'hall availability for customer' })
-    async availability() {
-        this.$logger.log("Called")
-        return
-    }
+  @Get('/availability')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth('AccessToken')
+  @Message('HALL.AVAILABILITY')
+  // @ApiOkResponse({ type: SendOtpResponseDto })
+  @ApiOperation({ summary: 'hall availability for customer' })
+  async availability() {
+    this.$logger.log("Called")
+    return
+  }
+
+  @Post('')
+  @Message('HALL.CREATE')
+  @ApiOkResponse({ type: CreateHallResponseDto })
+  @ApiOperation({ summary: 'Create hall' })
+  async create(@Body() createDto: CreateHallDto) {
+    return await this.$hallService.create(
+      createDto
+    );
+  }
+
 }
--- src/app/api/halls/hall.service.ts
+++ src/app/api/halls/hall.service.ts
@@ -1,6 +1,30 @@
 import { Injectable } from "@nestjs/common";
+import { CreateHallDto } from "./dto/create.dto";
+import { PrismaService } from "@app/databases/prisma/prisma.service";
+import { LoggerService } from "@app/shared/logger";
+import { OpenId } from "src/utils/open-id.util";
 
 @Injectable()
 export class HallService {
+
+    constructor(
+        private $logger: LoggerService,
+        private $prisma: PrismaService
+    ) {}
+
+    async create(payload: CreateHallDto) {
+        try {
+            await this.$prisma.hall.create({
+                data: {
+                    displayId: OpenId.format('HALL'),
+                    ...payload
+                }
+            })
+            return true;
+        } catch (err) {
+            this.$logger.error(err.message, err.stack);
+            throw err;
+        }
+    }
 
 }
