3a253bdd2915a234a0b36dbc3b1da133d9845d0a
pukhraj saini <pukhraj.saini@appinventiv.com>
2024-11-09 12:01:21 +0530

    IAuthUser decorator

--- src/app/decorators/user.decorator.ts
+++ src/app/decorators/user.decorator.ts
@@ -1,8 +1,9 @@
+import { IAuthUser } from '@app/api/auth/interfaces/auth-user';
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const User = createParamDecorator(
+export const AuthUser = createParamDecorator(
     (data: unknown, ctx: ExecutionContext) => {
         const request = ctx.switchToHttp().getRequest();
-        return request.user;
+        return request.user as IAuthUser;
     },
 );
