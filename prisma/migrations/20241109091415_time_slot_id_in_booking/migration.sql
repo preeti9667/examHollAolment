9f8384781d177a1c62f72ee840e0a82a06c41d02
pukhraj saini <pukhraj.saini@appinventiv.com>
2024-11-09 14:44:42 +0530

    chnaegs in schema:

--- prisma/migrations/20241109091415_time_slot_id_in_booking/migration.sql
+++ prisma/migrations/20241109091415_time_slot_id_in_booking/migration.sql
@@ -2,6 +2,7 @@
   Warnings:
 
   - You are about to drop the column `from` on the `Booking` table. All the data in the column will be lost.
+  - You are about to drop the column `timeSlotIds` on the `Booking` table. All the data in the column will be lost.
   - You are about to drop the column `to` on the `Booking` table. All the data in the column will be lost.
   - The `paymentMethod` column on the `Booking` table would be dropped and recreated. This will lead to data loss if there is data in the column.
   - The `status` column on the `Booking` table would be dropped and recreated. This will lead to data loss if there is data in the column.
@@ -9,6 +10,7 @@
 */
 -- AlterTable
 ALTER TABLE "Booking" DROP COLUMN "from",
+DROP COLUMN "timeSlotIds",
 DROP COLUMN "to",
 ADD COLUMN     "applicantName" TEXT,
 ADD COLUMN     "contact" JSONB,
@@ -17,10 +19,10 @@ ADD COLUMN     "institutionType" TEXT,
 ADD COLUMN     "noOfCandidates" INTEGER NOT NULL DEFAULT 0,
 ADD COLUMN     "organizationName" TEXT,
 ADD COLUMN     "startDate" TIMESTAMP(3),
+ADD COLUMN     "timeSlotId" TEXT,
 ALTER COLUMN "hallIds" SET DEFAULT ARRAY[]::TEXT[],
 ALTER COLUMN "examName" DROP NOT NULL,
 ALTER COLUMN "totalCost" SET DEFAULT 0,
-ALTER COLUMN "timeSlotIds" SET DEFAULT ARRAY[]::TEXT[],
 DROP COLUMN "paymentMethod",
 ADD COLUMN     "paymentMethod" TEXT,
 DROP COLUMN "status",
